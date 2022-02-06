a26700dbfccd16a101d75c4f369c52c470136d7c
Vincent <sggamecard@gmail.com>
2022-02-06 16:57:03 +0800

    fix env name

--- nodejs/dbs/index.js
+++ nodejs/dbs/index.js
@@ -1,13 +1,13 @@
 const MongoClient = require('mongodb').MongoClient
 
 const dbuser = process.env.DB_USER || 'root'
-const dbpassword = process.env.DB_PASSWORD
+const dbpassword = process.env.DB_PASS
 const dbhost = process.env.DB_HOST || 'localhost'
 const dbport = process.env.DB_PORT || 27017
 const dbname = process.env.DB_NAME || 'testdb'
 
 const DB_URI = "mongodb://" + dbuser + ":" + dbpassword + "@" + dbhost + ":" + dbport + "/" + dbname + "?authSource=admin"
-console.log('DB_URI=' + DB_URI)
+//console.log('DB_URI=' + DB_URI)
 function connect(url) {
   return MongoClient.connect(url, {
     connectTimeoutMS: 5000,
